d85d4f4c48dea7b3a9520df875674ff3a0847562
akhiranandha <kodamakhira624@gmail.com>
2024-05-21 19:55:07 +0530

    removeLike added

--- controller/controller.MovieUser.js
+++ controller/controller.MovieUser.js
@@ -34,14 +34,11 @@ async function addLike(req,res){
     try{
         const movieId=parseInt(req.params.id)
         const movie = await MovieModel.findById(movieId);
-        // console.log(movie.like.noOfLikes)
         if (!movie) {
           console.log('Movie not found');
         }
         var userId=parseInt(req.body.id)
         var user =await userModel.findById(userId)
-        // console.log(user)
-        
         movie.like.noOfLikes = movie.like.noOfLikes+1
         movie.like.likeduser.push(user)
         const updatedMovie = await MovieModel.findOneAndUpdate({_id:movieId},movie,{new:true});
@@ -53,4 +50,34 @@ async function addLike(req,res){
     }
 }
 
-module.exports={getAllMovies,getMovie,addLike}
+async function removeLike(req,res){
+    try{
+        const movieId=parseInt(req.params.id)
+        const movie = await MovieModel.findById(movieId);
+        if (!movie) {
+          console.log('Movie not found');
+        }
+        movie.like.noOfLikes = movie.like.noOfLikes-1
+        try{
+            var userId=parseInt(req.body.id)
+            var user =await userModel.findById(userId)
+            var ind = movie.like.likeduser.indexOf(user)
+            if(ind)
+                movie.like.likeduser.splice(ind,1)
+        }
+        catch(err){
+            console.log(err)
+            console.log("user not found");
+        }
+        const updatedMovie = await MovieModel.findOneAndUpdate({_id:movieId},movie,{new:true});
+        res.json(updatedMovie)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({message:"Error in liking movie"})
+    }
+}
+
+
+
+module.exports={getAllMovies,getMovie,addLike, removeLike}
--- routes/routes.moviesUser.js
+++ routes/routes.moviesUser.js
@@ -1,11 +1,12 @@
 const express=require("express")
 
-const {getAllMovies,getMovie,addLike}=require("../controller/controller.MovieUser")
+const {getAllMovies,getMovie,addLike, removeLike}=require("../controller/controller.MovieUser")
 
 const routes=express.Router()
 
 routes.get("/",getAllMovies)
 routes.get("/:id",getMovie)
 routes.put("/like/:id",addLike)
+routes.delete("/like/:id",removeLike)
 
 module.exports=routes
