b6c8ec26349bb1360a8671ede94d58235dbfdf4b
Ananya <ananyadhan13@gmail.com>
2024-05-23 16:18:38 +0530

    Frontend start

---
+++ Frontend/movieflix/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginComponent from './components/LoginComponent';
+import RegisterComponent from './components/RegisterComponent';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginComponent/>}></Route>
+      <Route path="/signup" element={<RegisterComponent/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
