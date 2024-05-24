3a41c3e4cba59003e08727f5bcdccf48ae63fc6f
Akhiranandha <kodamakhira624@gmail.com>
2024-05-24 15:50:42 +0530

    like

--- Frontend/src/components/SingleMovieComponent.js
+++ Frontend/src/components/SingleMovieComponent.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios'
-import { useParams } from 'react-router-dom';
-
+import { useParams,useNavigate } from 'react-router-dom';
 
 function SingleMovieComponent(){
     var [movie,setMovie] = useState({})
@@ -10,25 +9,44 @@ function SingleMovieComponent(){
         const {id} = useParams()
         const res =  await axios.get(`http://localhost:3001/api/movie/${id}`);
         setMovie(res.data)
+        setLike
+
     })
     function handleLike(id){
+        const navigate= useNavigate()
+        const token = localStorage.getItem("token")
+        axios.get(`http://localhost:3001/api/movie/like/${id}`,{ headers: {Authorization: `Bearer ${token}`}})
+            .then(res => {
+                if(res.data.status)
+                    navigate("/login")
+                else
+                    setLike(res.data.liked)
+            })
+
         if(!like){
-            // setLike(! like)
-            axios.put(`http://localhost:3001/api/movie/like/${id}`)
-                .then( res => 
-                    setLike(true)
-                )
+            axios.put(`http://localhost:3001/api/movie/like/${id}`,{ headers: {Authorization: `Bearer ${token}`}})
+                .then( res => {
+                    if(res.status===200)
+                        setLike(true)
+
+                })
                 .catch(err => console.log(err)) 
         }
         else{
-            axios.delete(`http://localhost:3001/api/movie/like/${id}`)
-                .then( res => 
-                    setLike(false)
-                )
+            axios.delete(`http://localhost:3001/api/movie/like/${id}`,{ headers: {Authorization: `Bearer ${token}`}})
+                .then( res => {
+                    if(res.status===200)
+                        setLike(true)
+                })
                 .catch(err => console.log(err)) 
         }
     }
+    function addComment(id){
+        const navigate= useNavigate()
+        const token = localStorage.getItem("token")
+        axios.put(`http://localhost:3001/api/comment/${id}`,)
 
+    }
     return (
         <>
 
