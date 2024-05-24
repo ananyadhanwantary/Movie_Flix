6f5626c4393440d33c748e0d69fe0a5fc62ce82b
VAditi18 <avuppugalla@gmail.com>
2024-05-24 19:23:36 +0530

    commit

--- Frontend/src/components/HeaderComponent.js
+++ Frontend/src/components/HeaderComponent.js
@@ -5,38 +5,33 @@ import { useNavigate } from "react-router-dom";
 
 function HeaderComponent() {
     const navigate = useNavigate()
-    async function handleLogin(e) {
+    function handleLogin(e) {
         e.preventDefault()
-        try {
-            navigate('/login')
-        }
-        catch(err){
-            console.log(err)
-        }
+        navigate('/login')
     }
-    async function handleSignup(e) {
+    function handleSignup(e) {
         e.preventDefault()
-        try {
-            navigate('/signup')
-        }
-        catch(err){
-            console.log(err)
-        }
+        navigate('/signup')
+    }
+    function handleHome(e) {
+        e.preventDefault()
+        navigate('/')
     }
     return (
         <>
-        <div className='d-flex container-fluid bg-dark'>
-            <div className='col-2 p-2 d-flex align-items-center poetsen-one-regular h1 text-white '>
-                <i className="bi bi-camera-reels-fill"></i> <span>MovieFlix</span>
-            </div>
-            <div className='container-fluid d-inline-flex flex-wrap flex-row-reverse py-2 align-items-center '>
-                <div className='float-right px-2'>
-                    <button className='btn btn-primary me-2'>Login</button>
-                    <button className='btn btn-primary'>Sign-up</button> 
+            <div className='d-flex container-fluid bg-dark'>
+                <div className='col-2 p-2 d-flex align-items-center poetsen-one-regular h1 text-white '
+                    onClick={(e) => { handleHome(e) }}>
+                    <i className="bi bi-camera-reels-fill"></i> <span>MovieFlix</span>
+                </div>
+                <div className='container-fluid d-inline-flex flex-wrap flex-row-reverse py-2 align-items-center '>
+                    <div className='float-right px-2'>
+                        <button className='btn btn-primary me-2' onClick={(e) => { handleLogin(e) }}>Login</button>
+                        <button className='btn btn-primary' onClick={(e) => { handleSignup(e) }}>Sign-up</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <hr className="p-0 m-0"/>
+            <hr className="p-0 m-0" />
         </>
     );
 }
--- Frontend/src/components/RegisterComponent.js
+++ Frontend/src/components/RegisterComponent.js
@@ -4,21 +4,29 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function RegisterComponent() {
-  const [user,setUser]=useState({
-    email:"",password:"",username:"",phone:""
+  const [user, setUser] = useState({
+    email: "", password: "", username: "", phone: ""
   })
-  const navigate=useNavigate()
-  async function handleSignup(e){
-    e.preventDefault()
-    try{
-      const response=await axios.post("http://localhost:3001/api/signup",user)
-      console.log(response)
-      alert("You have signed up successfully")
-      navigate('/login')
-    }
-    catch(err){
-      console.log(err)
-    }
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const handleSignup = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError('');
+    axios.post("http://localhost:3001/api/signup", user)
+      .then((res) => {
+        setLoading(false);
+        if (res.status === 200) {
+          alert(res.data.message)
+          navigate('/login');
+        } else {
+          setError(res.data.msg || 'Signup failed');
+        }
+      }).catch(err => {
+        setLoading(false);
+        setError('Error in Signup')
+      })
   }
   return (
     <div className="container mt-5">
@@ -29,56 +37,83 @@ function RegisterComponent() {
               <h3 className="text-center">Register</h3>
             </div>
             <div className="card-body">
-              <form>
+              <form onSubmit={(e) => handleSignup(e)}>
                 <div className="form-group">
                   <label>Email</label>
-                  <input type="text" value={user.email} className="form-control" placeholder="Enter email" id="email" required 
-                  onChange={(e)=>{
-                    setUser((user)=>{
-                      return {
-                        ...user,
-                        email:e.target.value
-                      }
-                    })
-                  }}/>
+                  <input
+                    type="email"
+                    value={user.email}
+                    className="form-control"
+                    placeholder="Enter email"
+                    required
+                    onChange={(e) => {
+                      setUser((user) => {
+                        return {
+                          ...user,
+                          email: e.target.value
+                        }
+                      })
+                    }} />
                 </div>
                 <div className="form-group">
                   <label>Password</label>
-                  <input type="password" value={user.password} className="form-control" placeholder="Password" id="password" required 
-                  onChange={(e)=>{
-                    setUser((user)=>{
-                      return {
-                        ...user,
-                        password:e.target.value
-                      }
-                    })
-                  }}/>
+                  <input
+                    type="password"
+                    value={user.password}
+                    className="form-control"
+                    placeholder="Password"
+                    required
+                    onChange={(e) => {
+                      setUser((user) => {
+                        return {
+                          ...user,
+                          password: e.target.value
+                        }
+                      })
+                    }} />
                 </div>
                 <div className="form-group">
                   <label>Username</label>
-                  <input type="text" value={user.username} className="form-control" placeholder="Enter username" id="username" required 
-                  onChange={(e)=>{
-                    setUser((user)=>{
-                      return {
-                        ...user,
-                        username:e.target.value
-                      }
-                    })
-                  }}/>
+                  <input
+                    type="text"
+                    value={user.username}
+                    className="form-control"
+                    placeholder="Enter username"
+                    required
+                    onChange={(e) => {
+                      setUser((user) => {
+                        return {
+                          ...user,
+                          username: e.target.value
+                        }
+                      })
+                    }} />
                 </div>
                 <div className="form-group">
                   <label>Phone number</label>
-                  <input type="tel" value={user.phone} className="form-control" placeholder="Enter phone number" id="phone" required 
-                  onChange={(e)=>{
-                    setUser((user)=>{
-                      return {
-                        ...user,
-                        phone:e.target.value
-                      }
-                    })
-                  }}/>
+                  <input
+                    type="tel"
+                    value={user.phone}
+                    className="form-control"
+                    placeholder="Enter phone number"
+                    required
+                    onChange={(e) => {
+                      setUser((user) => {
+                        return {
+                          ...user,
+                          phone: e.target.value
+                        }
+                      })
+                    }} />
                 </div>
-                <button className="btn btn-primary btn-block" onClick={(e)=>handleSignup(e)}>REGISTER</button>
+                {error && <div className="alert alert-danger">{error}</div>}
+                <div className="d-grid">
+                  <button
+                    type='submit'
+                    className="btn btn-primary btn-block"
+                    disabled={loading}
+                  >{loading ? 'Signing up...' : 'Sign up'}</button>
+                </div>
               </form>
             </div>
           </div>
