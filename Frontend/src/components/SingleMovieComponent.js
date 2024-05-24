a35abd84a278f6e4fbefbc4d6bd7f4319c2db289
Akhiranandha <kodamakhira624@gmail.com>
2024-05-24 17:40:19 +0530

    add like error

--- Frontend/src/components/SingleMovieComponent.js
+++ Frontend/src/components/SingleMovieComponent.js
@@ -11,43 +11,49 @@ function SingleMovieComponent() {
     const params = useParams()
     var [movie, setMovie] = useState({})
     var [like, setLike] = useState(false)
+    var [comment, setComment] = useState("")
     useEffect(() => {
         const { id } = params
         axios.get(`http://localhost:3001/api/movie/${id}`)
             .then(res => setMovie(res.data))
             .catch(err => console.log(err))
     })
-    function handleLike(id) {
+    async function handleLike(id) {
         const token = localStorage.getItem("token")
         console.log(token)
         if (token) {
-            axios.get(`http://localhost:3001/api/movie/like/${id}`, { headers: { Authorization: `Bearer ${token}` } })
-                .then(res => {
-                    if (res.data.status)
-                        navigate("/login")
-                    else
-                        setLike(res.data.liked)
-                })
-
+            var res = await axios.get(`http://localhost:3001/api/movie/like/${id}`,{ headers: {"Authorization" : `Bearer ${token}`} })
+            if (res.data.status)
+                navigate("/login")
+            else {
+                console.log(like)
+                setLike(res.data.liked)
+                console.log(like)
+            }
             if (!like) {
-                axios.put(`http://localhost:3001/api/movie/like/${id}`, { headers: { Authorization: `Bearer ${token}` } })
-                    .then(res => {
-                        if (res.status === 200) {
-                            setLike(true)
-                            document.getElementById("like_button").style.color = "red"
-                        }
-                    })
-                    .catch(err => console.log(err))
+                try{
+                    res = await axios.put(`http://localhost:3001/api/movie/like/${id}`, { headers: {"Authorization" : `Bearer ${token}`} })
+                    if (res.status === 200) {
+                        setLike(true)
+                        document.getElementById("like_button").style.color = "red"
+                    }
+                }
+                catch(err){
+                    console.log(err)
+                }
             }
             else {
-                axios.delete(`http://localhost:3001/api/movie/like/${id}`, { headers: { Authorization: `Bearer ${token}` } })
-                    .then(res => {
-                        if (res.status === 200) {
-                            setLike(true)
-                            document.getElementById("like_button").style.color = "black"
-                        }
-                    })
-                    .catch(err => console.log(err))
+                try{
+                    res = await axios.delete(`http://localhost:3001/api/movie/like/${id}`, { headers: {"Authorization" : `Bearer ${token}`} })
+                    if (res.status === 200) {
+                        setLike(true)
+                        document.getElementById("like_button").style.color = "black"
+                        console.log('hello else')
+                    }
+                }
+                catch(err){
+                    console.log(err)
+                }
             }
         }
         else {
@@ -57,11 +63,11 @@ function SingleMovieComponent() {
     }
     function addComment(id) {
         const token = localStorage.getItem("token")
-        axios.put(`http://localhost:3001/api/comment/${id}`,{comment: comment})
-            .then(res=>{
-                if(res.data.status)
+        axios.put(`http://localhost:3001/api/comment/${id}`, { comment: comment })
+            .then(res => {
+                if (res.data.status)
                     navigate("/login")
-                else{
+                else {
                     setMovie(res.data)
                     setComment("")
                 }
@@ -75,7 +81,7 @@ function SingleMovieComponent() {
                 <h1>{movie.movieName}</h1>
                 <p>{movie.movieCast}</p>
                 {/* <FontAwesomeIcon icon="fa-solid fa-heart"  onClick={handleLike}/>  */}
-                <AiFillLike onClick={handleLike} id="like_button" />
+                <AiFillLike onClick={() => handleLike(movie._id)} id="like_button" />
             </center>
         </>
     )
