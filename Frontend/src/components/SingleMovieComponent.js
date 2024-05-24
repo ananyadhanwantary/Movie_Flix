f631c1271775e17f5bda447cb88feff88449ec9c
Ananya <ananyadhan13@gmail.com>
2024-05-24 16:16:26 +0530

    commit

--- Frontend/src/components/SingleMovieComponent.js
+++ Frontend/src/components/SingleMovieComponent.js
@@ -1,64 +1,73 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios'
-import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { AiFillLike } from "react-icons/ai";
 //import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-import { useParams,useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
-function SingleMovieComponent(){
-    const params=useParams()
-    var [movie,setMovie] = useState({})
-    var [like,setLike] = useState(false)
-    useEffect(()=>{
-        const {id} = params
+function SingleMovieComponent() {
+    const navigate = useNavigate()
+    const params = useParams()
+    var [movie, setMovie] = useState({})
+    var [like, setLike] = useState(false)
+    useEffect(() => {
+        const { id } = params
         axios.get(`http://localhost:3001/api/movie/${id}`)
-        .then(res=>setMovie(res.data))
-        .catch(err => console.log(err))
+            .then(res => setMovie(res.data))
+            .catch(err => console.log(err))
     })
-    function handleLike(id){
-        const navigate= useNavigate()
+    function handleLike(id) {
         const token = localStorage.getItem("token")
-        axios.get(`http://localhost:3001/api/movie/like/${id}`,{ headers: {Authorization: `Bearer ${token}`}})
-            .then(res => {
-                if(res.data.status)
-                    navigate("/login")
-                else
-                    setLike(res.data.liked)
-            })
-
-        if(!like){
-            axios.put(`http://localhost:3001/api/movie/like/${id}`,{ headers: {Authorization: `Bearer ${token}`}})
-                .then( res => {
-                    if(res.status===200)
-                        setLike(true)
-
+        console.log(token)
+        if (token) {
+            axios.get(`http://localhost:3001/api/movie/like/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+                .then(res => {
+                    if (res.data.status)
+                        navigate("/login")
+                    else
+                        setLike(res.data.liked)
                 })
-                .catch(err => console.log(err)) 
+
+            if (!like) {
+                axios.put(`http://localhost:3001/api/movie/like/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+                    .then(res => {
+                        if (res.status === 200) {
+                            setLike(true)
+                            document.getElementById("like_button").style.color = "red"
+                        }
+                    })
+                    .catch(err => console.log(err))
+            }
+            else {
+                axios.delete(`http://localhost:3001/api/movie/like/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+                    .then(res => {
+                        if (res.status === 200) {
+                            setLike(true)
+                            document.getElementById("like_button").style.color = "black"
+                        }
+                    })
+                    .catch(err => console.log(err))
+            }
         }
-        else{
-            axios.delete(`http://localhost:3001/api/movie/like/${id}`,{ headers: {Authorization: `Bearer ${token}`}})
-                .then( res => {
-                    if(res.status===200)
-                        setLike(true)
-                })
-                .catch(err => console.log(err)) 
+        else {
+            navigate("/login")
         }
     }
-    function addComment(id){
-        const navigate= useNavigate()
+    function addComment(id) {
         const token = localStorage.getItem("token")
         axios.put(`http://localhost:3001/api/comment/${id}`,)
 
     }
     return (
         <>
-        <center>
-            <img src={movie.moviePosterUrl}></img>
-            <h1>{movie.movieName}</h1>
-            <p>{movie.movieCast}</p>
-             <FontAwesomeIcon icon="fa-solid fa-heart"  onClick={handleLike}/> 
-        </center>
+            <center>
+                <img src={movie.moviePosterUrl}></img>
+                <h1>{movie.movieName}</h1>
+                <p>{movie.movieCast}</p>
+                {/* <FontAwesomeIcon icon="fa-solid fa-heart"  onClick={handleLike}/>  */}
+                <AiFillLike onClick={handleLike} id="like_button" />
+            </center>
         </>
     )
 }
