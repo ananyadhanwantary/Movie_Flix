992c9ee8ed847ddab94f74bc8c41e9d0cbaa5a2a
VAditi18 <aditivuppugalla@gmail.com>
2024-06-01 21:00:41 +0530

    msg

--- Frontend/src/components/FooterComponent.js
+++ Frontend/src/components/FooterComponent.js
@@ -1,13 +1,12 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-// import { useNavigate } from "react-router-dom";
 
 function FooterComponent() {
   return (
     <>
     <hr className="p-0 m-0"/>
-    <footer className="footer d-flex bg-dark text-white py-2">
+    <footer className="footer d-flex bg-dark text-white py-2 fixed-bottom" style={{ height: "70px" }}>
       <div className="container d-flex justify-content-center align-content-center flex-column">
         <div className="column d-flex justify-content-center align-content-center ">
           <i className="bi bi-camera-reels-fill"></i>
--- Frontend/src/components/HeaderComponent.js
+++ Frontend/src/components/HeaderComponent.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
+import { IoPersonCircleSharp } from "react-icons/io5";
+
 
 function HeaderComponent() {
     const {token,logout} = useAuth()
@@ -29,7 +31,7 @@ function HeaderComponent() {
     }
     return (
         <>
-            <div className='d-flex container-fluid bg-dark'>
+            <div className='d-flex container-fluid bg-dark fixed-top' style={{ height: "60px" }}>
                 <div className='col-2 p-2 d-flex align-items-center poetsen-one-regular h1 text-white '
                     onClick={(e) => { handleHome(e) }}>
                     <i className="bi bi-camera-reels-fill"></i> <span>MovieFlix</span>
@@ -37,12 +39,12 @@ function HeaderComponent() {
                 <div className='container-fluid d-inline-flex flex-wrap flex-row-reverse py-2 align-items-center '>
                     {token==null ?
                        <div className='float-right px-2'>
-                        <button className='btn btn-primary me-2' onClick={(e) => { handleLogin(e) }}>Login</button>
+                        <button className='btn btn-light me-2' onClick={(e) => { handleLogin(e) }}>Login</button>
                         <button className='btn btn-primary' onClick={(e) => { handleSignup(e) }}>Sign-up</button>
                     </div>
                     :
                     <div className='float-right px-2'>
-                        <button className='btn btn-primary me-2' onClick={(e) => { handleProfile(e) }}>Profile</button>
+                        <IoPersonCircleSharp size={30} className="me-2" style={{ color: 'white' }} onClick={(e) => { handleProfile(e) }}/>
                         <button className='btn btn-primary' onClick={(e) => { handleLogout(e) }}>Logout</button>
                     </div>}
                 </div>
