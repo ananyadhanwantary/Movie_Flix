156369e08ce309fece308c62ad2415b7f3893146
akhiranandha <kodamakhira624@gmail.com>
2024-06-29 20:39:01 +0530

    profile page

--- Frontend/src/components/ProfileComponent.js
+++ Frontend/src/components/ProfileComponent.js
@@ -10,17 +10,17 @@ function ProfileComponent(){
         .then(res => setUser(res.data))
         .catch(err => console.log(err))
     },[userId])
-    // const imgPath = "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
+    const imgPath = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
     return(
         <>
         <br/><br/>
            <Container>
-                {/* <img src={imgPath} className="float-start rounded-circle " /> */}
-                <h1>{user.username}</h1>
-                <h1>Email:{user.email}</h1>
-                <h1>Phone Number:{user.phone}</h1>                
+                <img alt ="" src={imgPath} style={{height:"100px"}} className="float-start rounded-circle m-3" />
+                <h3>{user.username}</h3>
+                <h3>Email:{user.email}</h3>
+                <h3>Phone Number:{user.phone}</h3>                
            </Container>
-           <br/><br/><br/>
+           
         </>
     )
 }
